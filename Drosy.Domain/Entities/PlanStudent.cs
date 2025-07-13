3f595496d7b4eafade613d4f3052743c65d68651
Merk48 <meryKassis48@gmail.com>
2025-07-13 20:58:34 +0300

    feat(data): add PlanStudent entity and EF Core configuration

---
+++ Drosy.Domain/Entities/PlanStudent.cs
@@ -0,0 +1,21 @@
+﻿using Drosy.Domain.Interfaces.Common;
+
+namespace Drosy.Domain.Entities
+{
+    public class PlanStudent : ICreateAt
+    {
+        public int PlanId { get; set; }
+        public int StudentId { get; set; }
+        public string Fee { get; set; } = null!;
+        public string Notes { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } 
+
+        #region Nav Properties
+        public Student Student { get; set; } = null!;
+        public Plan Plan { get; set; } = null!;
+        #endregion
+    }
+
+}
+
+
---
+++ Drosy.Infrastructure/Validators/PlanStudentsValidator/AddStudentToPlanDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Drosy.Application.UseCases.PlanStudents.DTOs;
+using FluentValidation;
+namespace Drosy.Infrastructure.Validators.PlanStudentsValidator
+{
+    public class AddStudentToPlanDtoValidator : AbstractValidator<AddStudentToPlanDto>
+    {
+        public AddStudentToPlanDtoValidator()
+        {
+            RuleFor(x => x.StudentId)
+                .GreaterThan(0)
+                .WithMessage("Please specify a valid student.");
+
+            RuleFor(x => x.Notes)
+                .MaximumLength(500)
+                .WithMessage("Notes cannot exceed 500 characters.");
+        }
+    }
+}
