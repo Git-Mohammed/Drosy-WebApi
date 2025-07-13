1fce06f292d1d4e7017b64c9a6c384467c22d393
Merk48 <meryKassis48@gmail.com>
2025-07-13 22:53:58 +0300

    fix (audit): Add an intercepter for createdAt

---
+++ Drosy.Infrastructure/Persistence/Intercepters/AuditAtInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using Drosy.Domain.Interfaces.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Drosy.Infrastructure.Persistence.Intercepters
+{
+    public class AuditAtInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            SetAuditFields(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            SetAuditFields(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void SetAuditFields(DbContext? context)
+        {
+            if (context == null) return;
+            var now = DateTime.UtcNow;
+
+            foreach (var entry in context.ChangeTracker.Entries())
+            {
+                if (entry.Entity is ICreateAt created && entry.State == EntityState.Added)
+                    created.CreatedAt = now;
+
+                //if (entry.Entity is IUpdateAt updated && entry.State == EntityState.Modified)
+                //    updated.UpdatedAt = now;
+            }
+        }
+    }
+
+}
