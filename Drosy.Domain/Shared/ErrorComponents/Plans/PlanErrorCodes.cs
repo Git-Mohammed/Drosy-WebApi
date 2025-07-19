1878a100da15884b34286f6420d1a8ddb7e36cc8
Redaessa7 <redaesssa@gmail.com>
2025-07-19 12:30:41 +0300

    feat(errors): added multi-language messages for Plan validations, business rules, and repository errors

---
+++ Drosy.Domain/Shared/ErrorComponents/Plans/PlanErrorCodes.cs
@@ -0,0 +1,32 @@
+﻿namespace Drosy.Domain.Shared.ErrorComponents.Plans;
+
+public static class PlanErrorCodes
+{
+    #region Validations
+    
+    public const string StartDateRequired = "Error_Plan_StartDateRequired";
+    public const string EndDateRequired = "Error_Plan_EndDateRequired";
+    public const string StartSessionRequired = "Error_Plan_StartSessionRequired";
+    public const string EndSessionRequired = "Error_Plan_EndSessionRequired";
+    public const string TotalFeesMustBePositive = "Error_Plan_TotalFeesMustBePositive";
+    public const string InvalidSessionTimeRange = "Error_Plan_InvalidSessionTimeRange"; // start > end
+    public const string InvalidDateRange = "Error_Plan_InvalidDateRange"; // start > end
+    public const string DaysOfWeekRequired = "Error_Plan_DaysOfWeekRequired";
+    
+    #endregion
+    
+    #region Business Rule
+    public const string PlanTypeNotSupported = "Error_Plan_TypeNotSupported";
+    public const string PlanInactive = "Error_Plan_Inactive";
+    public const string SessionTimeConflict = "Error_Plan_SessionTimeConflict";
+    #endregion
+    
+    #region Data/Repository / EF Core / Constraint
+    
+    public const string PlanNotFound = "Error_Plan_NotFound";
+    public const string PlanCannotBeDeletedWithStudents = "Error_Plan_CannotDeleteWithStudents"; // لها طلاب مسجلين
+    public const string PlanSaveFailure = "Error_Plan_SaveFailure";
+    public const string ConstraintViolation = "Error_Plan_ConstraintViolation";
+    
+    #endregion
+}
---
+++ Drosy.Domain/Shared/ErrorComponents/Plans/PlanErrors.cs
@@ -0,0 +1,34 @@
+﻿namespace Drosy.Domain.Shared.ErrorComponents.Plans;
+
+public static class PlanErrors
+{
+    #region Validations
+
+    public static AppError StartDateRequired => new(PlanErrorCodes.StartDateRequired);
+    public static AppError EndDateRequired => new(PlanErrorCodes.EndDateRequired);
+    public static AppError StartSessionRequired => new(PlanErrorCodes.StartSessionRequired);
+    public static AppError EndSessionRequired => new(PlanErrorCodes.EndSessionRequired);
+    public static AppError TotalFeesMustBePositive => new(PlanErrorCodes.TotalFeesMustBePositive);
+    public static AppError InvalidSessionTimeRange => new(PlanErrorCodes.InvalidSessionTimeRange);
+    public static AppError InvalidDateRange => new(PlanErrorCodes.InvalidDateRange);
+    public static AppError DaysOfWeekRequired => new(PlanErrorCodes.DaysOfWeekRequired);
+
+    #endregion
+
+    #region Business Rules
+
+    public static AppError PlanTypeNotSupported => new(PlanErrorCodes.PlanTypeNotSupported);
+    public static AppError PlanInactive => new(PlanErrorCodes.PlanInactive);
+    public static AppError SessionTimeConflict => new(PlanErrorCodes.SessionTimeConflict);
+
+    #endregion
+
+    #region Data/Repository / EF Core / Constraint
+
+    public static AppError PlanNotFound => new(PlanErrorCodes.PlanNotFound);
+    public static AppError PlanCannotBeDeletedWithStudents => new(PlanErrorCodes.PlanCannotBeDeletedWithStudents);
+    public static AppError PlanSaveFailure => new(PlanErrorCodes.PlanSaveFailure);
+    public static AppError ConstraintViolation => new(PlanErrorCodes.ConstraintViolation);
+
+    #endregion
+}
