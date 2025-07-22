f67fd7948486424977d6b2b24771ce981b9c41c7
MohammedFares <xmohammedfares@gmail.com>
2025-07-22 15:32:32 +0300

    add sesstion mapping config

--- Drosy.Application/UseCases/Sessions/DTOs/SessionDTO.cs
+++ Drosy.Application/UseCases/Sessions/DTOs/SessionDTO.cs
@@ -5,7 +5,6 @@ namespace Drosy.Application.UseCases.Sessions.DTOs;
 public class SessionDTO
 {
     public int Id { get; set; }
-    public int PlanId { get; set; }
     public string Title { get; set; } = null!;
     public DateTime ExcepectedDate { get; set; }
     public DateTime StartTime { get; set; }
---
+++ Drosy.Infrastructure/Mapping/SesstionsConfigs/SessionMappingConfig.cs
@@ -0,0 +1,29 @@
+﻿using Drosy.Application.UseCases.Sessions.DTOs;
+using Drosy.Domain.Entities;
+using Mapster;
+
+namespace Drosy.Infrastructure.Mapping.SessionConfigs
+{
+    public class SessionMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            // DTO ➜ Entity
+            config.NewConfig<AddSessionDTO, Session>()
+                .Map(dest => dest.Title, src => src.Title.Trim())
+                .Map(dest => dest.PlanId, src => src.PlanId)
+                .Map(dest => dest.ExcepectedDate, src => src.ExcepectedDate)
+                .Map(dest => dest.StartTime, src => src.StartTime)
+                .Map(dest => dest.EndTime, src => src.EndTime);
+
+            // Entity ➜ DTO
+            config.NewConfig<Session, SessionDTO>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.Plan.Id, src => src.PlanId)
+                .Map(dest => dest.Title, src => src.Title)
+                .Map(dest => dest.ExcepectedDate, src => src.ExcepectedDate)
+                .Map(dest => dest.StartTime, src => src.StartTime)
+                .Map(dest => dest.EndTime, src => src.EndTime);
+        }
+    }
+}
