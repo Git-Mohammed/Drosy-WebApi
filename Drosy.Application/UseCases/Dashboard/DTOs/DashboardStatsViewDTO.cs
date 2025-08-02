370a9e43f66ef3499284b0fa653c5191c374cae1
Abdulaziz <bdalzyzalbrnawy47@gmail.com>
2025-08-02 07:10:05 +0300

    feat(dashboard): implement get dashboard

---
+++ Drosy.Application/UseCases/Dashboard/DTOs/DashboardStatsViewDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Drosy.Application.UseCases.Dashboard.DTOs;
+
+public class DashboardStatsViewDTO
+{
+    public int TotalRegisteredStudents { get; set; }
+    public decimal TotalAmountDue { get; set; }
+    public decimal TotalAmountCollected { get; set; }
+}
